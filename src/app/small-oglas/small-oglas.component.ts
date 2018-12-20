8afa31f80266097cd815d069b217b393bf016a82
StefanVelkoski <velkoskis@gmail.com>
2018-12-21 00:32:46 +0100

    with css-new

--- src/app/small-oglas/small-oglas.component.ts
+++ src/app/small-oglas/small-oglas.component.ts
@@ -7,11 +7,13 @@ import { HeroHomeComponent } from '../hero-home/hero-home.component';
   styleUrls: ['./small-oglas.component.css']
 })
 export class SmallOglasComponent implements OnInit {
-  medicinski = ['Windstorm', 'Bombasto', 'Magneta'];
-  edukativni = ['Windstorm', 'Bombasto', 'Magneta'];
-  itni = ['Windstorm', 'Bombasto', 'Magneta'];
-  heroes = ['Windstorm', 'Bombasto', 'Magneta'];
+  medicinski = ['Апел за помош на малиот Петар', 'Да му помогнеме на Борче', 'Апел за помош, заедно против канцерот'];
+  edukativni = ['Донација за младиот и ентузијастичен Марко', 'Со ваша помош до едукација од соништата', 'На чекор до остварувања без граници '];
+  itni = ['Помош на поплавените во Скопје', 'Да им помогнеме на погодените од земјотрес', 'Помош за оштетените во пожарот'];
+  memorijalni = ['Да се запамти делото на една мајка', 'Меморијален турнир во мал фудбал', 'Меморијален Концерт'];
   choices=['Surovina','Paricni sredstva'];
+  heroes=['Windstorm', 'Bombasto', 'Magneta'];
+
   myHero = this.heroes[0];
   constructor() { }
 
